f79e2c16f5d1de85072e43c7b451432e18a89142
Kentaro Kuribayashi <kentarok@gmail.com>
2013-03-10 02:28:31 +0900

    Added site-nginx

--- roles/app.rb
+++ roles/app.rb
@@ -1,2 +1,20 @@
 name "app"
-run_list %w(role[base] recipe[nginx])
+run_list %w(
+  role[base]
+  recipe[nginx]
+  recipe[site-nginx]
+)
+
+default_attributes(
+  "app" => {
+    "name"   => "prepan",
+    "domain" => "prepan.org",
+    "server" => {
+      "host" => "127.0.0.1",
+      "port" => "8000",
+    },
+    "nginx"  => {
+      "port" => "80",
+    },
+  }
+)
