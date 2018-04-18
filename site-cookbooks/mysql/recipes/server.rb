e96c174e2e11d668af107a961332151a6bf94cb0
shibayu36 <shibayu36@gmail.com>
2018-04-19 07:26:27 +0900

    remove mysql_install_db step, because it will be executed by init script

--- site-cookbooks/mysql/recipes/server.rb
+++ site-cookbooks/mysql/recipes/server.rb
@@ -4,12 +4,6 @@ package "mysql-server" do
   action :install
 end
 
-execute "mysql-install-db" do
-  command "mysql_install_db"
-  action  :run
-  not_if  { File.exists?('/var/lib/mysql/mysql/user.frm') }
-end
-
 service "mysqld" do
   supports status: true, restart: true, reload: true
   action   [ :enable, :start ]
